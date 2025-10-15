81b6fe54a0847b4db1ac6ffaf812404be1e25013
Kay Samuel <kaiperac@gmail.com>
2025-10-14 23:12:57 -0300

    primeiro commit

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/* 
+Projeto: Controle de estoque de papelaria
+Autor: [Kainã Samuel Ribeiro Cantanhede e João Paulo Gomes Nogueira]
+Data de criação: [14/10/2007]
+Descrição: Estrutura inicial do projeto integrador.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Gerenciador de estoque\n");
+    /*
+    futuro codigo para o projeto
+    */
+    return 0;
+}
